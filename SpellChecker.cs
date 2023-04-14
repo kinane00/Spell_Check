51eab2d9628ab0216c1fcde85fd8229eff4e7ba9
DESKTOP-JPKSBTC\LATITUDE <kinanemohammed@gmail.com>
2023-04-14 00:45:20 +0000

    Add project files.

---
+++ SpellChecker.cs
@@ -0,0 +1,57 @@
+﻿namespace TransGr8_DD_Test
+{
+	public class SpellChecker
+	{
+		private readonly List<Spell> _spellList;
+
+		public SpellChecker(List<Spell> spells)
+		{
+			_spellList = spells;
+		}
+
+		public bool CanUserCastSpell(User user, string spellName)
+		{
+			Spell spell = _spellList.Find(s => s.Name == spellName);
+			
+			if (user.Level < spell.Level)
+			{
+				return false;
+			}
+			if (spell.Components.Contains("V"))
+			{
+				if (!user.HasVerbalComponent)
+				{
+					return false;
+				}
+			}
+			else if (spell.Components.Contains("S"))
+			{
+				if (!user.HasSomaticComponent)
+				{
+					return false;
+				}
+			}
+			else if (spell.Components.Contains("M"))
+			{
+				if (!user.HasMaterialComponent)
+				{
+					return false;
+				}
+			}
+			if (user.Range < spell.Range)
+			{
+				return false;
+			}
+			if (spell.Duration.Contains("Concentration"))
+			{
+				if (!user.HasConcentration)
+				{
+					return false;
+				}
+			}
+			// Add additional checks as needed for specific saving throws or other requirements.
+			return true;
+		}
+		
+	}
+}
